6a3e58fef3a1a701993d4c9d493d933b379aa143
Jakub Ferdek <jakub.ferdek@gmail.com>
2021-12-13 00:07:11 +0100

    change structure and w/h scanner

--- scaner.php
+++ scaner.php
@@ -5,29 +5,29 @@ include 'php/navbar.php';
 
 <!-- code scanner qr -->
 <div class="container"></div>
-<section class="qrReaderSection h-100">
+<section class="qrReaderSection">
   <div class="row qrReaderSectionRow">
     <div class="col-12">
-      <div class="row parentOfCanvasTrue">
-        <div class="col-12"> <canvas id="canvas" class=""></canvas></div>
+      <div class="row parentOfCanvasRow">
+        <div class="col-12 wrapCanvas"> <canvas id="canvas" class=""></canvas></div>
       </div>
       <div class="row">
         <div class="col-12 inputPluParent">
           <div id="barcodes" class="border"></div>
           <input type="text" id="barcodesinput" class="border border-dark">
-          <input type="text" id="barcodesinputWithContent" class="border border-primary parentOfCanvas">
+          <input type="text" id="barcodesinputWithContent" class="border border-primary">
         </div>
       </div>
     </div>
 <!-- section choice camera -->
-    <div class="row  mt-5">
+    <!-- <div class="row  mt-5">
       <div class="col-12 mb-2">Wybierz Kamerę, z której chcesz korzystać</div>
       <div class="col-12 listDetectedCameraParent">
         <select name="" id="listDetectedCamera" class="border border-primary w-100">
-            <!-- <option value="1">asd</option> -->
+
         </select>
       </div>
-    </div>
+    </div> -->
 
   </div>
   </div>
