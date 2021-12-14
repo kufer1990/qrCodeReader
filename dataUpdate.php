181e6b9f3466b761092fb10cc28f118fe69b2a69
kufer1990 <94316752+kufer1990@users.noreply.github.com>
2021-12-14 19:42:56 +0100

    add upload file

--- dataUpdate.php
+++ dataUpdate.php
@@ -1,23 +1,39 @@
-<?php 
+<?php
 include 'php/header.php';
 // include 'php/navbar.php';
 
 ?>
+<div class="conainer">
+    <div class="row">
+        <div class="col">
+            <form enctype="multipart/form-data" action="excelTo.php" method="post">
+                <input type="hidden" name="MAX_FILE_SIZE" value="512000000" />
+                <input type="file" name="excelFileToConvert" />
+                <input type="submit" value="wyślij" />
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- 
 <div class="container">
 
-<div class="row">
-<form action="excelTo.php" method="post">
-<div class="col"></div>
-<div class="col"><input type="file" name="excelFileToConvert"></div>
-<div class="col"><button type="submit">Zatwierdź</button></div>
-</div>
-</form>
-</div>
+    <div class="row">
+        <form action="excelTo.php" method="post">
+            <div class="col"></div>
+            <div class="col"><input type="file" name="excelFileToConvert"></div>
+            <div class="col"><button type="submit">Zatwierdź</button></div>
+    </div>
+    </form>
+</div> -->
 
 </body>
 <script src="answear.js"></script>
+
 </html>
-<?php 
+<?php
 //  
 
 // include 'php/footer.php';
--- excelTo.php
+++ excelTo.php
@@ -1,38 +1,57 @@
 <?php
-$nameFileExcel = $_POST['excelFileToConvert'];
+$nameFileExcelTemp =  $_FILES['excelFileToConvert']['tmp_name'];
+$nameFileExcel = $_FILES['excelFileToConvert']['name'];
+
+$location = "excel/" . $_FILES['excelFileToConvert']['name'];
+
+if (is_uploaded_file($_FILES['excelFileToConvert']['tmp_name'])) {
+    if (!move_uploaded_file($_FILES['excelFileToConvert']['tmp_name'],  $location)) {
+        echo 'problem: Nie udało się skopiować pliku do katalogu.';
+        return false;
+    }
+} else {
+    echo 'problem: Możliwy atak podczas przesyłania pliku.';
+    echo 'Plik nie został zapisany.';
+    return false;
+}
+// return true;
+
+
+
+
 // header('refresh: .5;');
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
-require_once __DIR__.'/api/simplexlsx/src/SimpleXLSX.php';
-// header('refresh: .5;');
+require_once __DIR__ . '/api/simplexlsx/src/SimpleXLSX.php';
 
-$dateToTable="";
-if ( $xlsx = SimpleXLSX::parse('excel/'.$nameFileExcel)) {
+$dateToTable = "";
+if ($xlsx = SimpleXLSX::parse("excel/" . $nameFileExcel)) {
 
-	// Produce array keys from the array values of 1st array element
-	$header_values = $rows = [];
+    // Produce array keys from the array values of 1st array element
+    $header_values = $rows = [];
 
-	foreach ( $xlsx->rows() as $k => $r ) {
-		if ( $k === 0 ) {
-			$header_values = $r;
-			continue;
-		}
-		$rows[] = array_combine( $header_values, $r );
-	}
-    
-    //create question to sql
-    for ($i=0; $i <count($xlsx->rows())-1 ; $i++) {
-     if ($i<count($xlsx->rows())-2){
-  $dateToTable= $dateToTable."(".$rows[$i]['EAN'].",'".$rows[$i]['NAZWA']."',".$rows[$i]['STAN'].",".$rows[$i]['CENA_ZAKUPU'].",".$rows[$i]['CENA_SPRZEDAZY'].",'".$rows[$i]['MARKA']."','".$rows[$i]['DOST']."'),";
+    foreach ($xlsx->rows() as $k => $r) {
+        if ($k === 0) {
+            $header_values = $r;
+            continue;
         }
-     else{$dateToTable= $dateToTable."(".$rows[$i]['EAN'].",'".$rows[$i]['NAZWA']."',".$rows[$i]['STAN'].",".$rows[$i]['CENA_ZAKUPU'].",".$rows[$i]['CENA_SPRZEDAZY'].",'".$rows[$i]['MARKA']."','".$rows[$i]['DOST']."')";}
+        $rows[] = array_combine($header_values, $r);
+    }
+
+    //create question to sql
+    for ($i = 0; $i < count($xlsx->rows()) - 1; $i++) {
+        if ($i < count($xlsx->rows()) - 2) {
+            $dateToTable = $dateToTable . "(" . $rows[$i]['EAN'] . ",'" . $rows[$i]['NAZWA'] . "'," . $rows[$i]['STAN'] . "," . $rows[$i]['CENA_ZAKUPU'] . "," . $rows[$i]['CENA_SPRZEDAZY'] . ",'" . $rows[$i]['MARKA'] . "','" . $rows[$i]['DOST'] . "'),";
+        } else {
+            $dateToTable = $dateToTable . "(" . $rows[$i]['EAN'] . ",'" . $rows[$i]['NAZWA'] . "'," . $rows[$i]['STAN'] . "," . $rows[$i]['CENA_ZAKUPU'] . "," . $rows[$i]['CENA_SPRZEDAZY'] . ",'" . $rows[$i]['MARKA'] . "','" . $rows[$i]['DOST'] . "')";
+        }
     }
 
 
     include "php/connect.php";
-//create table
-$sql= "CREATE TABLE IF NOT EXISTS STANY (
+    //create table
+    $sql = "CREATE TABLE IF NOT EXISTS STANY (
     `EAN` BIGINT,
     `NAZWA` VARCHAR(30) CHARACTER SET utf8,
     `STAN` INT,
@@ -41,16 +60,16 @@ $sql= "CREATE TABLE IF NOT EXISTS STANY (
     `MARKA` VARCHAR(10) CHARACTER SET utf8,
     `DOST` VARCHAR(29) CHARACTER SET utf8
 );";
-$result = mysqli_query($conn, $sql );
+    $result = mysqli_query($conn, $sql);
 
-//clear table
-$sql2="DELETE FROM `STANY`";
-$result2=mysqli_query($conn, $sql2);
+    //clear table
+    $sql2 = "DELETE FROM `STANY`";
+    $result2 = mysqli_query($conn, $sql2);
 
-//add table to db
+    //add table to db
 
-$sql3="INSERT INTO STANY VALUES $dateToTable";
-$result3 = mysqli_query($conn, $sql3);
+    $sql3 = "INSERT INTO STANY VALUES $dateToTable";
+    $result3 = mysqli_query($conn, $sql3);
 
     // EAN
     // NAZWA
@@ -62,29 +81,29 @@ $result3 = mysqli_query($conn, $sql3);
 
 }
 // 
-include 'php/header.php';
-include 'php/navbar.php';
+// include 'php/header.php';
+// include 'php/navbar.php';
 
 ?>
 <style>
-.navbar{
-background-color: #198754!important;
+.navbar {
+    background-color: #198754 !important;
 }
-
-
 </style>
 
 
 <div class="container h-100 border border-dark">
     <div class="row h-100 d-flex align-items-center">
         <div class="col-12  d-flex align-items-center justify-content-center">
-            <div class="text justify-content-center text-white bg-success p-4">Gratulacje! Baza została zaktualizowana!</div>
+            <div class="text justify-content-center text-white bg-success p-4">Gratulacje! Baza została zaktualizowana!
+            </div>
         </div>
         <div class="col-12  d-flex align-items-center justify-content-center">
-            <button class="btn btn-outline-success"><a href="index.php" class="text-success text-decoration-none">Powróć do menu głównego</a></button>
+            <button class="btn btn-outline-success"><a href="index.php" class="text-success text-decoration-none">Powróć
+                    do menu głównego</a></button>
         </div>
     </div>
 </div>
-<?php 
+<?php
 include 'php/footer.php';
 ?>
