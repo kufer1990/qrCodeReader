0077e7228b7818a75779f5e166a5bf16425da68c
Jakub Ferdek <jakub.ferdek@gmail.com>
2021-12-22 23:17:39 +0100

    add return to log and remove number of company

--- js/create_account.js
+++ js/create_account.js
@@ -5,7 +5,7 @@ const shopNumber = document.querySelector('#shopNumber');
 const visiblebtnSubmit = document.querySelector('#btnSubmit');
 const unvisiblebtnSubmit = document.querySelector('#submit');
 const alertDiv = document.querySelector('.alertDangerCreateAccount');
-
+const returnToLog = document.querySelector('#returnToLog');
 
 
 
@@ -16,19 +16,25 @@ visiblebtnSubmit.addEventListener('click',()=>{
         if(password.value != passwordRepeat.value){
             alertDiv.style.display="block";
             alertDiv.textContent ="Spróbuj ponownie. Hasła nie są jednakowe";           
-        } else if(shopNumber.value!=7446){
+        // } else if(shopNumber.value!=){
  
-          alertDiv.style.display="block";
-          alertDiv.textContent ="Niepoprawny numer sklepu";
-        }else{
+        //   alertDiv.style.display="block";
+        //   alertDiv.textContent ="Niepoprawny numer sklepu";
+        }
+        else{
+            unvisiblebtnSubmit.click();
         //   wyslanie zapytania dodającego uzytkownika do bazy - ajax
         }
       
     }
+    else{
+        alertDiv.style.display="block";
+        alertDiv.textContent ="Uzupełnij Wszystkie Pola";
+    }
 })
 
 
-
-
-
+returnToLog.addEventListener('click',()=>{
+    window.location.href="panel_log.php";
+})
 
